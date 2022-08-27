4a4a277aa1fbf419484781a427f89f72a31186e7
Leonardo Galindo <galindocode@gmail.com>
2022-08-26 23:51:44 -0400

    vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://galindocode.github.io/first_project_with_actions/',
   plugins: [react()]
 })
